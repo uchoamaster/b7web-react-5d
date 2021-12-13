511c77d69c4ff31a6017d814bd49b5bca0b8f5d1
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-12-13 15:19:30 -0300

    criando item para armazenar lista fake

--- projeto1/todo/src/App.styles.ts
+++ projeto1/todo/src/App.styles.ts
@@ -6,3 +6,20 @@ export const Container = styled.div`
     min-height: 100vh;
 
 `;
+
+export const Area = styled.div`
+
+margin: auto;
+max-width: 980px;
+padding: 10px;
+
+`;
+
+export const Header =styled.h1`
+    margin:0;
+    padding:0;
+    color: #fff;
+    text-align:center;
+    border-bottom: 1px solid #444;
+    padding-bottom:20px;
+`;
--- projeto1/todo/src/App.tsx
+++ projeto1/todo/src/App.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import * as C from './App.styles';
-
+import { Item } from './types/item';
 const App = () => {
+//aqui eu falo que vou ter um array de item e o typescritp aceita
+const [list, setList] = useState<Item[]>([
+  {id: 1, name: 'Comprar pão na padaria', done: false},
+  {id: 2, name: 'Comprar um bolo na padaria', done: false},
+]);
+
   return(
 
     <C.Container>
-      ...
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+      </C.Area>
     </C.Container>
  
 
