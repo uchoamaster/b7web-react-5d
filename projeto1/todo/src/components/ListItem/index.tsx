2b67558bd8d97d425fefa756a2b43723a693d9b7
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-12-14 11:27:01 -0300

    aplicando logica para marcar e desmarcar

--- projeto1/todo/src/components/ListItem/index.tsx
+++ projeto1/todo/src/components/ListItem/index.tsx
@@ -13,8 +13,8 @@ export const ListItem = ({ item }: Props) => {
     const [isChecked, setIsChecked] = useState(item.done);
 
     return (
-        <C.Container>
-            <input type="checkbox" checked={isChecked} onChange={e => setIsChecked(e.target.checked)}/>
+        <C.Container done={isChecked}>
+            <input type="checkbox" checked={isChecked} onChange={event => setIsChecked(event.target.checked)}/>
             <label>{item.name}</label>
         </C.Container>
     );
--- projeto1/todo/src/components/ListItem/styles.ts
+++ projeto1/todo/src/components/ListItem/styles.ts
@@ -1,20 +1,27 @@
 import styled from 'styled-components';
 
-export const Container = styled.div `
-display: flex;
-background-color: #20212c;
-padding: 10px;
-border-radius: 10px;
-margin-bottom: 10px;
-align-items: center;
-
-input{
-    width: 25px;
-    height: 25px;
-    margin-right: 5px;
+type ContainerProps = {
+    done: boolean;
 }
 
-label{
-    color: #ccc;
-}
-`;
+export const Container = styled.div(({ done }: ContainerProps)=>(
+    `
+    display: flex;
+    background-color: #20212c;
+    padding: 10px;
+    border-radius: 10px;
+    margin-bottom: 10px;
+    align-items: center;
+    
+    input{
+        width: 25px;
+        height: 25px;
+        margin-right: 5px;
+    }
+    
+    label{
+        color: #ccc;
+        text-decoration: ${done ? 'line-through' : 'initial'};
+    }
+`
+));
