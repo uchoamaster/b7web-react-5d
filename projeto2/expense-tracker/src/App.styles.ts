c35c789ac1252704c061010872ac7e1bacd6ce50
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-12-14 15:32:19 -0300

    criando os componentes e iniciando a estilizaçao dos mesmos

--- projeto2/expense-tracker/src/App.styles.ts
+++ projeto2/expense-tracker/src/App.styles.ts
@@ -2,3 +2,9 @@ import styled from 'styled-components';
 
 
 export const Container = styled.div ``;
+
+export const Header = styled.div``;
+
+export const HeaderText = styled.h1``;
+
+export const Body = styled.div``;
--- projeto2/expense-tracker/src/App.tsx
+++ projeto2/expense-tracker/src/App.tsx
@@ -3,7 +3,12 @@ import * as C from './App.styles';
 const App = () => {
   return(
       <C.Container> 
-        ...
+        <C.Header>
+          <C.HeaderText>Sistema Financeiro</C.HeaderText>
+        </C.Header>
+        <C.Body>
+          ...
+        </C.Body>
       </C.Container>
   );
 }
