1300b0284bd2ba6eceba5317141176e12c4ea0c2
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-12-14 10:13:10 -0300

    criando a logica de deixar o checkbox clicavel

--- projeto1/todo/src/components/ListItem/index.tsx
+++ projeto1/todo/src/components/ListItem/index.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import  * as C from './styles';
 import { Item } from '../../types/Item';
+
 
 type Props = {
     item: Item
@@ -7,9 +9,12 @@ type Props = {
 
 
 export const ListItem = ({ item }: Props) => {
+
+    const [isChecked, setIsChecked] = useState(item.done);
+
     return (
         <C.Container>
-            <input type="checkbox" />
+            <input type="checkbox" checked={isChecked} onChange={e => setIsChecked(e.target.checked)}/>
             <label>{item.name}</label>
         </C.Container>
     );
