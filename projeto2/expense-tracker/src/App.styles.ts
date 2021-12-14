cc7f5c817f7989f5b4166cfc0ce0928fdcdc890b
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-12-14 16:00:00 -0300

    criando a estrutura toda estilizada

--- projeto2/expense-tracker/src/App.styles.ts
+++ projeto2/expense-tracker/src/App.styles.ts
@@ -3,8 +3,22 @@ import styled from 'styled-components';
 
 export const Container = styled.div ``;
 
-export const Header = styled.div``;
+export const Header = styled.div`
+    background-color: darkblue;
+    height: 150px;
+    text-align: center;
 
-export const HeaderText = styled.h1``;
+`;
 
-export const Body = styled.div``;
+export const HeaderText = styled.h1`
+margin:0;
+padding: 0;
+color: #fff;
+padding-top: 30px;
+`;
+
+export const Body = styled.div`
+    margin: auto;
+    max-width: 980px;
+    margin-bottom: 50px;
+`;
