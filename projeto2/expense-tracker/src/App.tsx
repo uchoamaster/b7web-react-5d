918cf0a7d9c3f70566c3bc02d6325325d24df248
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-12-15 11:23:38 -0300

    criando helpers, ajudadores, para determinadas funções no sistema

--- projeto2/expense-tracker/src/App.tsx
+++ projeto2/expense-tracker/src/App.tsx
@@ -1,6 +1,18 @@
+import { useState  } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { categories } from './data/categories';
+import { items } from './data/items';
+
+
+
 
 const App = () => {
+// lista completa dos anos
+  const [list, setList] = useState(items);
+  const [currentMonth, setCurrentMonth] = useState();
+
   return(
       <C.Container> 
         <C.Header>
